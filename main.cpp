31523c4757e678aed3ff84a64834f7146692dfed
ramprasad51 <ramprasad2007my@gmail.com>
2026-02-23 11:54:48 +0530

    C++ program to calculate the volume of boxes using class and object

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
-//Class and Object in C++ example program
+//Finding the volume of a box using class and object in C++.
 #include<iostream>
 using namespace std;
-class student{
+class Box{
     public:
-    int id;
-    string name;
+    double breadth;
+    double length;
+    double height;
 };
 
 int main()
-{
-    student s1;//object of class student
-    s1.id=51;
-    s1.name="RAM PRASAD";
-    cout<<s1.id<<endl;
-    cout<<s1.name<<endl;
-    return 0;
+{//
+    Box Box1;
+    Box Box2;
+    double volume=0.00;
+    Box1.height=5.0;
+    Box1.length=6.0;
+    Box1.breadth=7.0;
+    Box2.height=10.0;
+    Box2.length=12.0;
+    Box2.breadth=13.0;
+    volume=Box1.height*Box1.length*Box1.breadth;
+    cout<<"Volume of Box1: "<<volume<<endl;
+     volume=Box2.height*Box2.length*Box2.breadth; 
+      cout<<"Volume of Box2: "<<volume<<endl;
+      return 0;
+
+
 }
