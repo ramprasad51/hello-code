2c328041f99ab136407e5dd954fe2d7c916d52fd
ramprasad51 <ramprasad2007my@gmail.com>
2026-03-21 14:17:21 +0530

    Implement area calculation for rectangle and triangle

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,6 +1,11 @@
-#divisible by 3 or 5 but not both
-for i in range(1,101):
-    if i%3==0 and i%5==0:
-        continue
-    if i%3==0 or i%5==0:
-        print(i,end="   ")
+#Program to calculate area of rectangle and triangle 
+length=float(input("Enter length of rectangle: "))
+breadth=float(input("Enter breadth of rectangle: "))
+area_rect=length*breadth
+
+base=float(input("Enter base of a traingle: "))
+height=float(input("Enter height of traingle: "))
+area_tri=0.5*base*height
+#Displays the area result 
+print("Area of Rectangle: ",area_rect)
+print("Area of Triangle: ",area_tri)
