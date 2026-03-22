8ad8acdfcd5f825565b534ee9d06714059c0c822
ramprasad51 <ramprasad2007my@gmail.com>
2026-03-22 20:03:24 +0530

    Factorial of a number(PYTH0N)

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,11 +1,9 @@
-#Program to calculate area of rectangle and triangle 
-length=float(input("Enter length of rectangle: "))
-breadth=float(input("Enter breadth of rectangle: "))
-area_rect=length*breadth
+#Programm to calculate factor!al of a number
+n=int(input("Enter a number: "))
+i=1
+fact=1
+while i<=n:
+    fact=fact*i
+    i+=1
 
-base=float(input("Enter base of a traingle: "))
-height=float(input("Enter height of traingle: "))
-area_tri=0.5*base*height
-#Displays the area result 
-print("Area of Rectangle: ",area_rect)
-print("Area of Triangle: ",area_tri)
+print("Factorial=",fact)    
