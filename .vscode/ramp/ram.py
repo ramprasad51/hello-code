20217e499b8f8e9d2ffd4ca64bf35faec0ae41f7
ramprasad51 <ramprasad2007my@gmail.com>
2026-03-29 22:01:56 +0530

    Implement function to remove vowels from a name

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,9 +1,16 @@
-#Programm to calculate factor!al of a number
-n=int(input("Enter a number: "))
-i=1
-fact=1
-while i<=n:
-    fact=fact*i
-    i+=1
+#Program to remove vowels from a name
+def remove_vowels(name):#function to remove vowels from the name
+    result=""  #to store name without vowels
 
-print("Factorial=",fact)    
+    for ch in name:#iterate through each character in the name
+        if ch not in "aeiouAEIOU":#check if the character is not a vowel
+            result+=ch
+    return result
+
+name=input("Enter your name:")
+
+#function call
+output=remove_vowels(name)
+#Display name without v0wElS
+print("Name after removing vowels:",output)
+    
