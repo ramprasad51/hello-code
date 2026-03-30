0cec49e2a4bab3a19a34ba5dc4adcec4b1003d10
ramprasad51 <ramprasad2007my@gmail.com>
2026-03-30 22:26:49 +0530

    class to demonstrate encapsulation

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
-// accessing  members using "this" keyword 
+//Programm to dmonstrate the ENCAPSULATION in C++
 #include <iostream>
 using namespace std;
 
-class Test{
+class Example{
+    private:
+    int secret; // Private member variable, not accessible outside the class
     public:
-    string teststring;
-    void setData(string Teststring){
-        this->teststring = Teststring; // using "this" keyword to access the member variable
+    void setSecret(int value){// Public member function to set the value of the private member variable
+        secret = value;
     }
-    void getandPrint(string str)
-    {
-        this->setData(str); // using "this" keyword to call the member function
-        cout << "The string is: " << this->teststring << endl; 
+    int getSecret(){// Public member function to get the value of the private member variable
+        return secret;
     }
 };
 
-int main(){
-    Test t;
-    t.getandPrint("This is a test member access using \"this\" keyword!!!");
+int main() {
+    Example obj;// Create an object of the Example class
+    obj.setSecret(84);// Set the value of the private member variable using the public member function
+    cout << "The secret value is: " << obj.getSecret() << endl;
     return 0;
 }
