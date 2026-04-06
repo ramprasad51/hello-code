c019bda60f054bf2808a29cb1418e15b4c78714b
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-06 22:30:30 +0530

    Product of 2numbers by filehandling

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
-//programm to understand exception handling
+//Multiplying  2numbers by the process of FileHandling
+#include<fstream>//for file handling
 #include<iostream>
 using namespace std;
 int main(){
-    try{
-        throw 45.0;
+    int num1,num2;
+    ifstream inputFile("input.txt");//creating an object of ifstream class 
+    if(inputFile.is_open()){
+        inputFile>>num1>>num2;//reading 2 numbers from the file
+        inputFile.close(); 
+    }else{
+        cerr<<"Unable to open input.txt";//error handling
+        return 1;
     }
-    catch (int i){
-        cout<<"An integer: "<<i<<endl;
+    int result=num1*num2;
+    ofstream outputFile("output.txt");//creating an object of ofstream class
+    if(outputFile.is_open()){
+        outputFile<<result;
+        outputFile.close();
+    }else{
+        cerr<<"Unable to open output file";//error handling
+        return 1;
     }
-    catch(double d){
-        cout<<"An floating: "<<d<<endl;
-    }
+    cout<<"Result  written in output.txt: "<<result<<endl;
     return 0;
 }
