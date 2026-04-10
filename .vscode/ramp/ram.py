c79d81fb5d95b2dcc994621e77cead8fcf22d372
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-10 21:11:11 +0530

    Reversing of string

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,10 +1,19 @@
-#Checking PALINDROME of a string
-str=input("Enter a string: ")
+#Reversing of the string by using slicing
+str=input("Enter the string: ")
+print("The reversed string is: ",str[::-1])
 
-if str==str[::-1]:       #Reversing the string and comparing it with the original string done using slicing [::-1] means to reverse the string
-    print("The string is a palindrome.")    
-else:    
-    print("The string is not a palindrome.")
+str1=input("Enter another string: ")
+#Reversing of the string using for loop
+reversed_str=""
+for i in str1:
+    reversed_str=i+reversed_str
+print("The reversed string is: ",reversed_str)
 
-
-
+#Reversing of the string using while loop
+str2=input("Enter another string: ")
+reversed_str2=""
+i=len(str2)-1   
+while i>=0:
+    reversed_str2=reversed_str2+str2[i]
+    i-=1
+print("The reversed string is: ",reversed_str2)
