08deb9410c3db07b7f325c940b5dda0f4cd2f400
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-10 19:28:21 +0530

    list and string indexing examples

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,28 +1,10 @@
-#COMMON STRING METHODS/OPERATIONS
-str="Where are you?"
-caps_str=str.upper()  #converts the string to uppercase
-print(caps_str)
-str1="HELLO DUNIYA"
-small_str=str1.lower() #converts the string to lowercase
-print(small_str)
-n="1234"
-print(n.isdigit())    #checks if the string contains only digits
-a="ROCK"
-print(a.isalpha())   #checks if the string contains only alphabets
-b="ROCK123"
-print(b.isalnum())  #checks if the string contains only alphabets and numeric characters
-sp="    "
-print(sp.isspace()) #checks if the string contains only whitespace characters
-sub_str="Banana"
-print(sub_str.find("na")) #finds the first occurrence of the specified substring and returns its index, or -1 if not found
-print(sub_str.find("na",3)) #Starts searching for the substring "na" from index 3 and returns the index of the first occurrence
-l="Hello World Welcome to Python"
-print(l.split())   #Splits the string into a list of substrings based on whitespace by default
-rep="Picks"
-print(rep.replace("P","Tr")) #Replaces all occurrences of the specified old substring with the new substring
-rip="HOW ARE YOU?"
-print(rip.strip()) #Removes leading and trailing whitespace from the string & special characters
-f="JAMES BOND"
-print(f.startswith("J")) #checks if the string starts with the specified substring
-print(f.endswith("E")) #checks if the string ends with the specified substring
+#ACESSING ELEMENTS IN A LIST & STRING USING INDEXING
+Fruit="Mango"
+print(Fruit[0])  #Accesses the first character of the string
+print(Fruit[2])  #Accesses the third character of the string
+#Negative Indexing
+print(Fruit[-1]) #Accesses the last character of the string
 
+Prime_num=[2,3,5,7,11]
+print(Prime_num[0]) #Accesses the first element of the list
+print(Prime_num[-2]) #Accesses the fourth element of the list
