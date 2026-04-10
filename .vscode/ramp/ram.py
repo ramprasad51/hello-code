45414ee2513d0bd49b8bbafad6b14dbdf9cc2bc8
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-10 19:37:16 +0530

    length and indexing of string

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,10 +1,14 @@
-#ACESSING ELEMENTS IN A LIST & STRING USING INDEXING
-Fruit="Mango"
-print(Fruit[0])  #Accesses the first character of the string
-print(Fruit[2])  #Accesses the third character of the string
-#Negative Indexing
-print(Fruit[-1]) #Accesses the last character of the string
+#LENGTH & INDEXING
+Word="Banana!"
+print(len(Word))
+print(Word[0])
+print(Word[1])
+print(Word[2])
+print(Word[3])
+print(Word[4])
+print(Word[5])
+print(Word[6])
 
-Prime_num=[2,3,5,7,11]
-print(Prime_num[0]) #Accesses the first element of the list
-print(Prime_num[-2]) #Accesses the fourth element of the list
+'''RELATION BETWEEN LeNGTH & INDEXING
+len()=last index + 1
+last item is always at the index len()-1  '''
