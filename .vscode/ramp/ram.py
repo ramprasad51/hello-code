5ce76ee16fda7680425f4763dcab06c9b390de85
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-11 13:19:56 +0530

    demonstrate string formatting with placeholders

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,23 +1,20 @@
-#Converting the upper case to lower case and vice versa
-str=input("Enter the string: ")
-result=""
-for i in str:
-    if i.isupper():
-        result+=i.lower()
-    elif i.islower():
-        result+=i.upper()
-    else:
-        result+=i
-print("The converted string is:", result)
+#String formatting
 
-#Manually converting the upper case to lower case and vice versa
-str1=input("Enter another string: ")
-result1=""
-for i in str1:
-    if i>='A' and i<='Z':
-        result1+=chr(ord(i)+32)
-    elif i>='a' and i<='z':
-        result1+=chr(ord(i)-32)
-    else:
-        result1+=i
-print("The converted string is:", result1)
+#Done by using the .format() method and placeholders in the string
+
+#Single placeholder/value
+phrase="His name is {0}!".format("Ram Prasad")
+print(phrase)
+#Multiple placeholders/values
+Name="Ram Prasad"
+Age=19
+phrase2="His name is {0} and his age is {1}.".format(Name,Age)
+print(phrase2)
+#Swapping the order of the placeholders
+phrase3="His name is {1} and his age is {0}.".format(Name,Age)
+print(phrase3)
+#Format specifications
+x=4
+y=5
+phrase4="2**10={0} & {1}*{2}={3:.3f}".format(2**10,x,y,x*y)
+print(phrase4)
