0adfad32a1e6ba9eb1d3047560e09fc2423246e1
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-11 12:26:57 +0530

    Implement case conversion for strings

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,5 +1,23 @@
-#Count words using string split method
-str=input("Enter a string: ")
-words=str.split()            #split() method splits the string into a list of words based on whitespace
-print("The words in the string are:", words)
-print("Number of words in the string:", len(words))
+#Converting the upper case to lower case and vice versa
+str=input("Enter the string: ")
+result=""
+for i in str:
+    if i.isupper():
+        result+=i.lower()
+    elif i.islower():
+        result+=i.upper()
+    else:
+        result+=i
+print("The converted string is:", result)
+
+#Manually converting the upper case to lower case and vice versa
+str1=input("Enter another string: ")
+result1=""
+for i in str1:
+    if i>='A' and i<='Z':
+        result1+=chr(ord(i)+32)
+    elif i>='a' and i<='z':
+        result1+=chr(ord(i)-32)
+    else:
+        result1+=i
+print("The converted string is:", result1)
