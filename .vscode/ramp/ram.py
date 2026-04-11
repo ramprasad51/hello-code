ea3ba84c97768a08ce6f95c69ae09567b063e9fd
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-11 06:20:56 +0530

    Implement vowel counting in a string

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,7 +1,7 @@
-#Checking whether the substring is present in the string or not
-str=input("Enter the string: ")
-sub_str=input("Enter the substring: ")
-if sub_str in str:                     #Using the 'in' operator to check for the presence of the substring in the string
-    print("The substring is present in the string.")
-else:
-    print("The substring is not present in the string.")
+#Counting vowels in a string
+str=input("Enter a string: ")
+count=0
+for i in str:                
+    if i in 'aeiouAEIOU':     #using 'in' operator to check vowels present in the string
+        count+=1
+print("The number of vowels in the string is:", count)
