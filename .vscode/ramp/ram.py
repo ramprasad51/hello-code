bffd202862a4ec6df75aa72bb3b562162217e267
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-11 11:49:07 +0530

    Substring replacement

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,12 +1,7 @@
-#Removing vowels from a string
-def remove_vowels(phrase):
-    vowels = 'aeiouAEIOU'
-    result = ''
-    for char in phrase:
-        if char not in vowels:    #The 'not in' operator checks if the character is not a vowel
-            result += char      #If the character is not a vowel, it is added to the result string
-    return result
-#Example usage
-phrase=input("Enter a phrase: ")
-result = remove_vowels(phrase)  #Calling the function to remove vowels from the input phrase
-print("Phrase without vowels:", result)
+#Replacing substring from string
+'''Since Strings are immutable in Python, we cannot change the original string directly. However, we can create a new string with the desired modifications.
+ We can use the replace() method to replace a substring with another substring.'''
+str=input("Enter a string:")
+new_str=str.replace("a","x")
+
+print("Modified string:",new_str)
