bdb68b20a5b4babbcfae12804de0bce062296999
ramprasad51 <ramprasad2007my@gmail.com>
2026-04-11 06:11:19 +0530

    String_comprasion

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,19 +1,7 @@
-#Reversing of the string by using slicing
-str=input("Enter the string: ")
-print("The reversed string is: ",str[::-1])
-
-str1=input("Enter another string: ")
-#Reversing of the string using for loop
-reversed_str=""
-for i in str1:
-    reversed_str=i+reversed_str
-print("The reversed string is: ",reversed_str)
-
-#Reversing of the string using while loop
-str2=input("Enter another string: ")
-reversed_str2=""
-i=len(str2)-1   
-while i>=0:
-    reversed_str2=reversed_str2+str2[i]
-    i-=1
-print("The reversed string is: ",reversed_str2)
+#String comprasion
+str1=input("Enter the first string: ")
+str2=input("Enter the second string: ")
+if str1==str2:                           #Comparing the two strings by using the equality operator (==) if the strings are equal, it will return True, otherwise it will return False.
+    print("The strings are equal.")
+else:
+    print("The strings are not equal.")
