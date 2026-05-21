aaa5e8a174d72b1c21185a9b16aa3e4790a1f912
ramprasad51 <ramprasad2007my@gmail.com>
2026-05-21 07:06:16 +0530

    Student grade tracker

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,21 +1,35 @@
-#String formatting
+#STUDENT GRADE TRACKER
+#Take input
+n=int(input("Enter number of students:"))
+students=[]    #Empty list to store students data
+for i in range(n):
+    print("Enter student deatils",i+1)
+    name=input("Name:")
+    marks=float(input("Marks:"))
+    student={"name":name,"marks":marks}      #Displaying student deatils in the form of dicitionaries
+    students.append(student)                 #Appending dicitionary data to list
 
-'''Done by using the .format() method and placeholders in the string
-The placeholders{0} mean the first argument passed to the .format() method, {1} means the second argument and so on.'''
+#Calculate Average
+all_marks=[]
+for s in students:
+    all_marks.append(s["marks"])
+avg=sum(all_marks)/len(all_marks)
 
-#Single placeholder/value
-phrase="His name is {0}!".format("Ram Prasad")
-print(phrase)
-#Multiple placeholders/values
-Name="Ram Prasad"
-Age=19
-phrase2="His name is {0} and his age is {1}.".format(Name,Age)
-print(phrase2)
-#Swapping the order of the placeholders
-phrase3="His name is {1} and his age is {0}.".format(Name,Age)
-print(phrase3)
-#Format specifications
-x=4
-y=5
-phrase4="2**10={0} & {1}*{2}={3:.3f}".format(2**10,x,y,x*y)
-print(phrase4)
+#find ToPPER
+topper=students[0]
+for s in students:
+    if s["marks"]>topper["marks"]:
+        topper=s
+#find Lowest scorer
+lowest=students[0]
+for s in students:
+    if s["marks"]<lowest["marks"]:
+        lowest=s
+#DISPLAY SUMMARY
+print("\n---SUMMARY REPORT---")
+print(student)
+print("average:",avg)
+print("Topper:",topper["name"],"-",topper["marks"])
+print("Lowest scorer:",lowest["name"],"-",lowest["marks"])
+
+
