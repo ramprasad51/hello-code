79d5e443109bd6ee56d519516560d697138f473e
ramprasad51 <ramprasad2007my@gmail.com>
2026-05-21 07:46:45 +0530

    Exception handling

--- .vscode/ramp/ram.py
+++ .vscode/ramp/ram.py
@@ -1,35 +1,18 @@
-#STUDENT GRADE TRACKER
-#Take input
-n=int(input("Enter number of students:"))
-students=[]    #Empty list to store students data
-for i in range(n):
-    print("Enter student deatils",i+1)
-    name=input("Name:")
-    marks=float(input("Marks:"))
-    student={"name":name,"marks":marks}      #Displaying student deatils in the form of dicitionaries
-    students.append(student)                 #Appending dicitionary data to list
-
-#Calculate Average
-all_marks=[]
-for s in students:
-    all_marks.append(s["marks"])
-avg=sum(all_marks)/len(all_marks)
-
-#find ToPPER
-topper=students[0]
-for s in students:
-    if s["marks"]>topper["marks"]:
-        topper=s
-#find Lowest scorer
-lowest=students[0]
-for s in students:
-    if s["marks"]<lowest["marks"]:
-        lowest=s
-#DISPLAY SUMMARY
-print("\n---SUMMARY REPORT---")
-print(student)
-print("average:",avg)
-print("Topper:",topper["name"],"-",topper["marks"])
-print("Lowest scorer:",lowest["name"],"-",lowest["marks"])
-
-
+#EXCEPTION HANDLING
+def DivExp(a,b):
+    assert a>0,"a must be greater than 0"
+    if b==0:
+        raise ZeroDivisionError("Division by zero is not allowed")
+    c=a/b
+    return c
+a=float(input("Enter value for a:"))
+b=float(input("Enter value for b:"))
+try:
+    result=DivExp(a,b)
+    print("Result of a/b:",result)
+except AssertionError as ae:
+    print("AssertionError:",ae)
+except ZeroDivisionError as ze:
+    print("Exception:",ze)
+    
+   
